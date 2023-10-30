c8835ed885479b6062521681e4343530468296bf
Narnia11 <narges.hekmatnia1983@gmail.com>
2023-10-30 10:44:53 +0100

    Add Dto

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -15,28 +15,51 @@ public class ProductsController : ControllerBase
     this.context = context;
   }
 
-    [HttpGet]
-    public IEnumerable<Product> GetProducts([FromQuery] string? productName)
+  //GET https://localhost:8000/products
+  //GET https://localhost:8000/products?productName={productName}
+  [HttpGet]
+  public IEnumerable<ProductDto> GetProducts([FromQuery] string? productName)
+  {
+    var products = productName is null
+        ? context.Products.ToList()
+        : context.Products.Where(x => x.ProductName.Contains(productName)).ToList();
+
+var productsDto = products.Select(products => new ProductDto
     {
-        var products = productName is null 
-            ? context.Products.ToList()
-            : context.Products.Where(x => x.ProductName.Contains(productName)).ToList();
+      Id = products.Id,
+      ProductName = products.ProductName,
+      SerialNum = products.SerialNum,
+      ProductDesc = products.ProductDesc,
+      ImageUrl = products.ImageUrl,
+      Price = products.Price
+    });
 
-        return products;
-    }
+    return productsDto;
+  }
 
+  //GET https://localhost:8000/products/{sku}
   [HttpGet("{serialNum}")]
-  public ActionResult<Product> GetProduct(string serialNum)
+  public ActionResult<ProductDto> GetProduct(string serialNum)
   {
     var product = context.Products.FirstOrDefault(x => x.SerialNum == serialNum);
 
     if (product is null)
-    {
       return NotFound();
-    }
-    return product;
+
+      var productDto = new ProductDto
+    {
+      Id = product.Id,
+      ProductName = product.ProductName,
+      SerialNum = product.SerialNum,
+      ProductDesc = product.ProductDesc,
+      ImageUrl = product.ImageUrl,
+      Price = product.Price
+    };
+    
+    return productDto;
   }
 
+  //POST https://localhost:8000/products
   [HttpPost]
   public IActionResult CreateProduct(Product product)
   {
@@ -45,12 +68,25 @@ public class ProductsController : ControllerBase
 
     context.SaveChanges();
 
+    var productDto = new ProductDto
+    {
+      Id = product.Id,
+      ProductName = product.ProductName,
+      SerialNum = product.SerialNum,
+      ProductDesc = product.ProductDesc,
+      ImageUrl = product.ImageUrl,
+      Price = product.Price
+    };
+
+    //return Created("", productDto); // 201 Created
+////////////////////////////////////////////////////////////
     return CreatedAtAction(
       nameof(GetProduct),
       new { serialNum = product.SerialNum },
       product);
   }
 
+  //DELETE https://localhost:8000/products/{sku}
   [HttpDelete("{serialNum}")]
   public IActionResult DeleteProduct(string serialNum)
   {
@@ -65,6 +101,16 @@ public class ProductsController : ControllerBase
     context.SaveChanges();
 
     return NoContent(); // 204 No Content
+  }
+
+  public class ProductDto
+  {
+    public int Id { get; set; }
+    public string ProductName { get; set; }
+    public string SerialNum { get; set; }
+    public string ProductDesc { get; set; }
+    public string ImageUrl { get; set; }
+    public int Price { get; set; }
   }
 }
 
